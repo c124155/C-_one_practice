aa440ff0b5093bdc644845428c369637294d711f
c124155 <iqraibrahim460@gmail.com>
2026-09-24 03:56:25 -0700

    .

---
+++ Àssignment_1/Àssignment_1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Àssignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Iblname = new System.Windows.Forms.Label();
+            this.Iblstudentid = new System.Windows.Forms.Label();
+            this.Ibldepartment = new System.Windows.Forms.Label();
+            this.Iblsemester = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtstudentid = new System.Windows.Forms.TextBox();
+            this.txtdepartment = new System.Windows.Forms.TextBox();
+            this.txtsemester = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.btnshowinfo = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Iblname
+            // 
+            this.Iblname.AutoSize = true;
+            this.Iblname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iblname.Location = new System.Drawing.Point(125, 134);
+            this.Iblname.Name = "Iblname";
+            this.Iblname.Size = new System.Drawing.Size(259, 29);
+            this.Iblname.TabIndex = 0;
+            this.Iblname.Text = "Enter the student name";
+            // 
+            // Iblstudentid
+            // 
+            this.Iblstudentid.AutoSize = true;
+            this.Iblstudentid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iblstudentid.Location = new System.Drawing.Point(128, 195);
+            this.Iblstudentid.Name = "Iblstudentid";
+            this.Iblstudentid.Size = new System.Drawing.Size(222, 29);
+            this.Iblstudentid.TabIndex = 1;
+            this.Iblstudentid.Text = "Enter the student ID";
+            // 
+            // Ibldepartment
+            // 
+            this.Ibldepartment.AutoSize = true;
+            this.Ibldepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ibldepartment.Location = new System.Drawing.Point(128, 257);
+            this.Ibldepartment.Name = "Ibldepartment";
+            this.Ibldepartment.Size = new System.Drawing.Size(237, 29);
+            this.Ibldepartment.TabIndex = 2;
+            this.Ibldepartment.Text = "Enter the department";
+            // 
+            // Iblsemester
+            // 
+            this.Iblsemester.AutoSize = true;
+            this.Iblsemester.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iblsemester.Location = new System.Drawing.Point(128, 319);
+            this.Iblsemester.Name = "Iblsemester";
+            this.Iblsemester.Size = new System.Drawing.Size(215, 29);
+            this.Iblsemester.TabIndex = 3;
+            this.Iblsemester.Text = "Enter the semester";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(402, 134);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(335, 30);
+            this.txtname.TabIndex = 4;
+            // 
+            // txtstudentid
+            // 
+            this.txtstudentid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtstudentid.Location = new System.Drawing.Point(402, 195);
+            this.txtstudentid.Name = "txtstudentid";
+            this.txtstudentid.Size = new System.Drawing.Size(335, 30);
+            this.txtstudentid.TabIndex = 5;
+            // 
+            // txtdepartment
+            // 
+            this.txtdepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdepartment.Location = new System.Drawing.Point(402, 260);
+            this.txtdepartment.Name = "txtdepartment";
+            this.txtdepartment.Size = new System.Drawing.Size(335, 30);
+            this.txtdepartment.TabIndex = 6;
+            // 
+            // txtsemester
+            // 
+            this.txtsemester.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsemester.Location = new System.Drawing.Point(402, 323);
+            this.txtsemester.Name = "txtsemester";
+            this.txtsemester.Size = new System.Drawing.Size(335, 30);
+            this.txtsemester.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 46);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "STUDENT INFORMATION";
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbloutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbloutput.Location = new System.Drawing.Point(128, 390);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(609, 77);
+            this.lbloutput.TabIndex = 9;
+            // 
+            // btnshowinfo
+            // 
+            this.btnshowinfo.Location = new System.Drawing.Point(128, 500);
+            this.btnshowinfo.Name = "btnshowinfo";
+            this.btnshowinfo.Size = new System.Drawing.Size(185, 59);
+            this.btnshowinfo.TabIndex = 11;
+            this.btnshowinfo.Text = "Show information";
+            this.btnshowinfo.UseVisualStyleBackColor = true;
+            this.btnshowinfo.Click += new System.EventHandler(this.btnshowinfo_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(552, 500);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(185, 59);
+            this.btnexit.TabIndex = 12;
+            this.btnexit.Text = "Exist";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(340, 500);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(185, 59);
+            this.btnclear.TabIndex = 13;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 613);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnshowinfo);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsemester);
+            this.Controls.Add(this.txtdepartment);
+            this.Controls.Add(this.txtstudentid);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.Iblsemester);
+            this.Controls.Add(this.Ibldepartment);
+            this.Controls.Add(this.Iblstudentid);
+            this.Controls.Add(this.Iblname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Iblname;
+        private System.Windows.Forms.Label Iblstudentid;
+        private System.Windows.Forms.Label Ibldepartment;
+        private System.Windows.Forms.Label Iblsemester;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtstudentid;
+        private System.Windows.Forms.TextBox txtdepartment;
+        private System.Windows.Forms.TextBox txtsemester;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.Button btnshowinfo;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Button btnclear;
+    }
+}
+
---
+++ Àssignment_1/Àssignment_1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Àssignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnshowinfo_Click(object sender, EventArgs e)
+        {
+            // create variable of initial
+            string std_name = txtname.Text;
+            string std_id = txtstudentid.Text;
+            string std_dept = txtdepartment.Text;
+            string std_semester = txtsemester.Text;
+
+            // concatinating the vraibles
+            string student = std_name + std_id + std_dept + std_semester;
+
+            // displaying the output
+            lbloutput.Text = student;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            // clearing the inputs and outputs
+            txtname.Clear();
+            txtstudentid.Clear();
+            txtdepartment.Clear();
+            txtsemester.Clear();
+
+            lbloutput.Text = "";
+
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
